68cd73d82342b1ebf21341d047fae763d99b85cc
unknown <aswatth56@gmail.com>
2024-06-19 08:48:51 +0530

    Moved from config file to env

--- main.go
+++ main.go
@@ -1,97 +1,35 @@
 package main
 
 import (
-	"context"
 	"course-registration-system/course-service/controllers"
-	"course-registration-system/course-service/models"
 	"course-registration-system/course-service/services"
-	"encoding/json"
-	"fmt"
 	"log"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
-type Database struct {
-	DB_TYPE              string
-	DB_NAME              string
-	DB_CONNECTION_STRING string
-}
+func main() {
 
-type Config struct {
-	PORT int
-	DB   []Database
-}
-
-func (config *Config) LoadConfig(file_name string) {
-
-	file, err := os.Open(file_name)
+	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error occured while reading config file.", err)
+		log.Fatal("Error loading .env file")
 	}
 
-	decoder := json.NewDecoder(file)
-
-	err = decoder.Decode(&config)
-	if err != nil {
-		log.Fatal("Error occured while reading config file.", err)
-	}
-}
-
-func ConnectToDB(config Config) (services.MongoDatabase, services.MySqlDatabase) {
 	sql_database := new(services.MySqlDatabase)
-	mongo_database := new(services.MongoDatabase)
+	sql_database.Connect(os.Getenv("MYSQL_CONNECTION_STRING"))
 
-	for _, database := range config.DB {
-		if database.DB_TYPE == "MONGO" {
-			mongo_database.Connect(context.Background(), database.DB_CONNECTION_STRING)
-			defer mongo_database.Disconnect(context.Background())
-			mongo_database.Ping(context.Background())
-			mongo_database.SetDatabase(database.DB_NAME)
-			fmt.Println("Connected to Mongo DB")
-		} else if database.DB_TYPE == "MYSQL" {
-			sql_database.Connect(database.DB_CONNECTION_STRING)
-			fmt.Println("Connected to MySQL DB")
-		}
-	}
-
-	return *mongo_database, *sql_database
-}
-
-func InitializeCourseCrud(mongo_db services.MongoDatabase, mysql_db services.MySqlDatabase) *controllers.CourseCrudController {
 	course_service := new(services.CourseCrudService)
-	course_service.Init(mysql_db)
+	course_service.Init(*sql_database)
 
 	course_controller := new(controllers.CourseCrudController)
 	course_controller.Init(*course_service)
 
-	return course_controller
-}
-
-func main() {
-
-	//Load config
-	config := new(Config)
-	config.LoadConfig("config.json")
-
-	mongo_database, sql_database := ConnectToDB(*config)
-
-	course_crud_controller := InitializeCourseCrud(mongo_database, sql_database)
-
 	server := gin.Default()
 
 	base_path := server.Group("")
-	course_crud_controller.RegisterRoutes(base_path)
+	course_controller.RegisterRoutes(base_path)
 
-	course := new(models.Course)
-	course.CreateCourse(101, "Pqr", 4, "intro to pqr", "CS")
-	// fmt.Println(course)
-
-	server.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	server.Run(":" + fmt.Sprint(config.PORT)) // listen and serve on 0.0.0.0:8080
+	server.Run(":" + os.Getenv("PORT")) // listen and serve on 0.0.0.0:8080
 }
